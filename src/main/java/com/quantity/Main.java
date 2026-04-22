bd75a6046bbb3fd46165287436515840c520f554
Abhilash Ghosh <your-email@example.com>
2026-04-22 10:04:22 +0530

    Initial project setup

---
+++ src/main/java/com/quantity/Main.java
@@ -0,0 +1,10 @@
+package com.quantity;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Quantity Measurement Application");
+    }
+}
