4e344b201cfa9d49f034f4488db64e3a8148b5ef
Abhilash Ghosh <your-email@example.com>
2026-04-22 10:11:36 +0530

    UC2: Add immutable Inches value

--- src/main/java/com/quantity/Inches.java
+++ src/main/java/com/quantity/Inches.java
@@ -1,4 +1,13 @@
 package com.quantity;
 
 public final class Inches {
+    private final double value;
+
+    public Inches(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
