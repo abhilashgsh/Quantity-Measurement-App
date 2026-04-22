b1b810b25bba1c2d918584421b0d36ab9a684866
Abhilash Ghosh <your-email@example.com>
2026-04-22 10:08:48 +0530

    UC1: Add immutable Feet value

--- src/main/java/com/quantity/Feet.java
+++ src/main/java/com/quantity/Feet.java
@@ -1,4 +1,13 @@
 package com.quantity;
 
 public final class Feet {
+    private final double value;
+
+    public Feet(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
